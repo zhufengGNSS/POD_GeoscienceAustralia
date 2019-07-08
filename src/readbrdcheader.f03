85a0a2e0e60a2d00bad5ead8b546e0f238a7f9c9
Tzupang <tzupang.tseng@ga.gov.au>
2019-07-08 11:51:21 +1000

    Modified the makefile and other two major codes for the broadcast program updates

--- src/readbrdcheader.f03
+++ src/readbrdcheader.f03
@@ -43,18 +43,19 @@ SUBROUTINE readbrdcheader (UNIT_IN, VERSION, IONALPHA, IONBETA, A0UTC, A1UTC, &
 ! Dummy arguments declaration
 ! ----------------------------------------------------------------------
 ! IN
-      CHARACTER (LEN=100), INTENT(IN) :: UNIT_IN
+      INTEGER (KIND = prec_int4), INTENT(IN) :: UNIT_IN
 ! OUT
-      INTEGER (KIND = prec_int2),INTENT(OUT) :: VERSION
-      INTEGER (KIND = prec_int8),INTENT(OUT) :: LEAP, NWKUTC
+      INTEGER (KIND = prec_int4),INTENT(OUT) :: VERSION
+      INTEGER (KIND = prec_int4),INTENT(OUT) :: LEAP, NWKUTC, ITUTC
       REAL (KIND = prec_d),DIMENSION(4),INTENT(OUT)::IONALPHA, IONBETA 
-      REAL (KIND = prec_d),INTENT(OUT):: A0UTC,A1UTC,ITUTC
+      REAL (KIND = prec_d),INTENT(OUT):: A0UTC,A1UTC
 ! ----------------------------------------------------------------------
       CHARACTER (LEN=100) :: filename				
-      INTEGER (KIND = prec_int8) :: i,j 
-      INTEGER (KIND = prec_int2) :: UNIT_IN, ios
-      INTEGER (KIND = prec_int2) :: ios_line
+      INTEGER (KIND = prec_int4) :: i,j 
+      INTEGER (KIND = prec_int4) :: ios
+      INTEGER (KIND = prec_int4) :: ios_line
       CHARACTER (LEN=7) :: Format1
+      CHARACTER (LEN=100) :: HEAD
       CHARACTER (LEN=500) :: line_ith	  
       CHARACTER (LEN=150) :: word1_ln, word_i, t0	  
 ! ----------------------------------------------------------------------
@@ -71,6 +72,7 @@ i = i + 1
 ! Read the first line
 IF (i .EQ. 1) THEN
 READ (line_ith(1:6),'(I6)') VERSION
+PRINT*,'RINEX VERIOSN=',VERSION
 READ (line_ith(61:80),'(A20)') HEAD
 IF(VERSION .LE. 0) &
 PRINT*,'RINEX VERIOSN=',VERSION,'UNEXPECTED VALUE'
@@ -83,12 +85,16 @@ READ (line_ith(61:80),'(A20)') HEAD
 
 IF      (HEAD .EQ. 'ION ALPHA           ') THEN
 READ(line_ith(1:50),'(2X,4D12.4)') (IONALPHA(j),j=1,4)
+PRINT*,'ION ALPHA =',IONALPHA(1),IONALPHA(2),IONALPHA(3),IONALPHA(4)
 ELSE IF (HEAD .EQ. 'ION BETA            ') THEN
 READ(line_ith(1:50),'(2X,4D12.4)') (IONBETA(j),j=1,4)
+PRINT*,'ION BETA =',IONBETA(1),IONBETA(2),IONBETA(3),IONBETA(4)
 ELSE IF (HEAD .EQ. 'DELTA-UTC: A0,A1,T,W') THEN
 READ(line_ith(1:59),'(3X,2D19.12,2I9)') A0UTC,A1UTC,ITUTC,NWKUTC
+PRINT*,'DELTA-UTC: A0,A1,T,W',A0UTC,A1UTC,ITUTC,NWKUTC
 ELSE IF (HEAD .EQ. 'LEAP SECONDS        ') THEN
 READ(line_ith,'(I6)') LEAP
+PRINT*,'LEAP SECONDS',LEAP
 ELSE IF (HEAD .EQ. 'END OF HEADER       ') THEN
 PRINT*,'START TO READ THE ORBITAL PARAMETERS'
 RETURN
--- src/readbrdcmessg.f90
+++ src/readbrdcmessg.f90
@@ -1,11 +1,27 @@
-SUBROUTINE  readbrdcmessg (UNIT_IN,ISAT,IREC,EPH,CLK)
+SUBROUTINE  readbrdcmessg(UNIT_IN,VERSION,MAXNSAT,MAXNPAR,MAXEPO, &
+                          IYEAR4,MONTH,IDAY,ISAT,EPH,CLK)
 
 
 ! ----------------------------------------------------------------------
 ! MODULE: readbrdcmessg.f90 
 ! ----------------------------------------------------------------------
-! Purpose: Read the orbit and clock parameters from broadcast ephemeris 
-!          denotes the year 
+! Purpose: Read the orbit and clock parameters from broadcast ephemeris.
+!
+! INPUT:
+!       UNIT_IN : File number
+!       VERSION : RINEX version
+!       MAXNSAT : Maximum number of satellites
+!       MAXNPAR : Maximum number of parameters from broadcast file
+!       MAXEPO  : Maximum number of epochs recorded in broadcast file
+!       IYEAR4  : 4-digit year
+!       MONTH   : month
+!       IDAY    : day
+!
+! OUTPUT:
+!       ISAT    : Satellite PRN number
+!       EPH     : Array containing the broadcast orbital message
+!       CLK     : Array containing the broadcast clock message 
+!           
 ! ----------------------------------------------------------------------
 ! Author :      Dr. Tzupang Tseng
 !
@@ -15,55 +31,140 @@ SUBROUTINE  readbrdcmessg (UNIT_IN,ISAT,IREC,EPH,CLK)
 ! ----------------------------------------------------------------------
       USE mdl_precision
       USE mdl_num
-      USE mdl_planets
       USE m_shadow
       USE mdl_param
-      USE m_reformbrdc
       IMPLICIT NONE
 
 
 !-------------------------------------------------------------------
-      INTEGER (KIND = prec_int4) :: prnnum
-      INTEGER (KIND = 4)         :: satsvn
-      REAL (KIND = prec_d), INTENT(IN) :: mjd
-      REAL (KIND = prec_d), DIMENSION(3), INTENT(IN) :: rsat, vsat
-      REAL (KIND = prec_q), INTENT(OUT) :: beta
-      REAL (KIND = prec_q) :: lambda, del_u, yaw
-!--------------------------------------------------------------------
-      DOUBLE PRECISION  JD, Zbody(6)
-      INTEGER (KIND = 4) :: i, j
-      REAL (KIND = prec_q), DIMENSION(3) :: rbody
-      REAL (KIND = prec_q), DIMENSION(3) :: rSun, rMoon
-      REAL (KIND = prec_q), DIMENSION(3) :: r_sun1, r_sun2
-      REAL (KIND = prec_q) :: u_sun, yaw2
-      INTEGER  NTARG_SUN, NTARG_MOON, NCTR
-      REAL (KIND = prec_q), DIMENSION(3) :: ed, ey, eb, ex, en, ev, ez, et
-      REAL (KIND = prec_q), DIMENSION(3) :: yy
-      REAL (KIND = prec_q), DIMENSION(9) :: kepler
-      REAL (KIND = prec_q), DIMENSION(4) :: cosang
-      REAL (KIND = prec_q) :: R11(3,3),R33(3,3)
-      REAL (KIND = prec_q) :: Pi, Ps, AU, sclfa
-      REAL (KIND = prec_d) :: GM, Ds
-      REAL (KIND = prec_d) :: u_sat, i_sat, omega_sat
+      INTEGER (KIND = prec_int4),INTENT(IN) :: UNIT_IN
+      INTEGER (KIND = prec_int4),INTENT(IN) :: VERSION
+      INTEGER (KIND = prec_int4),INTENT(IN) :: MAXNSAT, MAXNPAR,MAXEPO
+      INTEGER (KIND = prec_int4),INTENT(OUT) :: IYEAR4,MONTH,IDAY
+      INTEGER (KIND = prec_int4),INTENT(OUT) :: ISAT
+      REAL (KIND = prec_q),INTENT(OUT) :: EPH(MAXNPAR,MAXEPO,MAXNSAT),CLK(MAXNPAR,MAXEPO,MAXNSAT)
+      REAL (KIND = prec_q) :: EPHDAT(MAXNPAR)
+      REAL (KIND = prec_q) :: EPHV3(MAXNPAR),CLKV3(MAXNPAR)
+
+      INTEGER (KIND = prec_int4) :: ISVN
+! ----------------------------------------------------------------------
+      INTEGER (KIND = prec_int4) :: I,J,K,IEPO
+      INTEGER (KIND = prec_int4) :: N2,IOS_LINE
+      INTEGER (KIND = prec_int2) :: iSec
+      INTEGER (KIND = prec_int2) :: iSvn2
+      INTEGER (KIND = prec_int2) :: LIMYR
+      INTEGER (KIND = prec_int4) :: IYEAR, IHOUR,MINUTE
+      REAL (KIND = prec_q)       :: SEC
+      REAL (KIND = prec_q)       :: LIMYR2, LIMYR0
+      REAL (KIND = prec_q)       :: DAY, TOC
+      CHARACTER (LEN=7) :: FORMAT1
+      CHARACTER (LEN=7) :: cSvn
+      CHARACTER (LEN=500) :: line_ith
+! ----------------------------------------------------------------------
+!      INTEGER IY, IM, ID
+      DOUBLE PRECISION MJD0, MJD
+      REAL (KIND = prec_q)       :: Z, T
+      REAL (KIND = prec_d)       :: GPS_wsec, GPS_day
+      INTEGER (KIND = prec_int8) :: GPS_week, GPSweek_mod1024
       INTEGER (KIND = prec_int2) :: AllocateStatus
-      INTEGER              :: PD_Param_ID
+      DATA LIMYR/1960/
 
+EPHDAT = 0.d0
+EPH    = 0.d0
+CLK    = 0.d0
+EPHV3  = 0.d0
+CLKV3  = 0.d0
 
-
-K=0
 ! Read broadcast message 
 !-------------------------------
 
-CALL brdcinfo (UNIT_IN, VERSION, ISVN, EPHDAT)
+IF(VERSION.EQ.1) THEN
+   N2=25
+ELSE
+   N2 = MAXNPAR
+ENDIF
 
-CALL reformbrdc (EPHDAT,EPHV3,CLKV3)
+IEPO = 0
+DO WHILE(.TRUE.)
+! Read input file
+FORMAT1 = '(A80)'
+READ (UNIT=UNIT_IN,FMT=FORMAT1,IOSTAT=IOS_LINE) line_ith
 
-K=K+1
+   ! ERROR MESSAGE HANDLING
+   IF (IOS_LINE > 0)  THEN
+       print*,'SOMETHING WRONG! PLEASE CHECK FILE OR VARIABLES USED IN PROGRAM '
+   ELSE IF (IOS_LINE < 0) THEN
+       print*,'END OF BROADCAST ORBIT FILE'
+       EXIT
+   ELSE
+!      print*,line_ith
+   END IF
+     ! -----------------------
 
-DO I=1,20
-EPH(I,ISVN)=EPHV3(I)
-CLOCK(I,ISVN)=CLKV3(I)
+! Read the orbital parameters
+IF(VERSION.LT.3) THEN
+READ(line_ith,'(I2,5I3,F5.1,3D19.12)') ISVN,IYEAR,MONTH,IDAY,IHOUR,MINUTE,SEC,&
+                                     (EPHDAT(K),K=2,4)
+
+IEPO = IEPO + 1
+ISAT = ISVN
+!PRINT*,'IEPO =', IEPO
+!PRINT*,'ISAT =', ISAT
+!print*,'IEPO=', IEPO, line_ith
+       ! Convert 2-digit year to 4-digit year
+         LIMYR2 = MOD(LIMYR,100)
+         LIMYR0 = (LIMYR/100)*100
+         IYEAR4 = IYEAR
+         IF (IYEAR4.LT.LIMYR2) IYEAR4=IYEAR4+LIMYR0+100
+         IF (IYEAR4.LT.100)    IYEAR4=IYEAR4+LIMYR0
+
+ELSE IF(VERSION.EQ.3) THEN
+READ(line_ith,'(A1,I2,I5,5I3,3D19.12)') cSvn,iSvn2,IYEAR,MONTH,IDAY,IHOUR,&
+                                     MINUTE,iSec,(EPHDAT(K),K=2,4)
+IEPO = IEPO + 1
+
+       ! Convert 2-digit year to 4-digit year
+         LIMYR2 = MOD(LIMYR,100)
+         LIMYR0 = (LIMYR/100)*100
+         IYEAR4 = IYEAR
+         IF (IYEAR4.LT.LIMYR2) IYEAR4=IYEAR4+LIMYR0+100
+         IF (IYEAR4.LT.100)    IYEAR4=IYEAR4+LIMYR0
+
+! Convert the cSvn to the ACS internal prn??
+ELSE
+PRINT*,'THE RINEX VERSION OF BROADCAST EPHEMERIS CANNOT BE RECOGNIZED!'
+END IF
+
+!print*,'TIME LINE =', IYEAR4, MONTH ,IDAY
+
+! Convert the calender time to MJD time system
+CALL iau_CAL2JD ( IYEAR4, MONTH, IDAY, MJD0, MJD, J )
+!print*,'MJD0 =', MJD0
+!print*,'MJD =, J =',MJD, J
+TOC = MJD + (IHOUR/24.D0+MINUTE/1440.D0+SEC/86400.D0)
+!print*,'TOC =', TOC
+
+
+! Convert MJD to the GPS week
+CALL time_GPSweek2 (TOC , GPS_week, EPHDAT(1), GPSweek_mod1024, GPS_day)
+!print*,'GPS_week=, GPS_day=',GPS_week,GPS_day
+!print*,'EPHDAT(1-4) =',EPHDAT(1),EPHDAT(2),EPHDAT(3),EPHDAT(4)
+
+DO  I=5,N2,4
+READ (UNIT=UNIT_IN,FMT=FORMAT1,IOSTAT=IOS_LINE) line_ith
+!print*,line_ith
+
+READ(line_ith,'(3X,4D19.12)') (EPHDAT(K),K=I,I+3)
+!PRINT*,'EPHDAT=',  EPHDAT(I),EPHDAT(I+1),EPHDAT(I+2),EPHDAT(I+3)
+
 END DO
 
+CALL reformbrdc (EPHDAT,EPHV3,CLKV3)
+    DO I=1,20
+    EPH(I,IEPO,ISAT)=EPHV3(I)
+!print*,'EPH(1:2,:,:), GPSWEEK, TOE', EPH(1,IEPO,ISAT), EPH(2,IEPO,ISAT)
+    CLK(I,IEPO,ISAT)=CLKV3(I)
+    END DO
 
+END DO
 END SUBROUTINE  
