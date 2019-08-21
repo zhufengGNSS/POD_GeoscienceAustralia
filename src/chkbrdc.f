adca77f199b4a47a974515a2736b7b86073d98d7
Tzupang <tzupang.tseng@ga.gov.au>
2019-08-21 06:35:25 +0000

    ACS-264: add function of quality check for the broadcast program

--- src/chkbrdc.f
+++ src/chkbrdc.f
@@ -1,162 +1,168 @@
-      SUBROUTINE chkbrdc(EPH1,EPH2)
+      SUBROUTINE chkbrdc(EPH,AVE,STD)
 CC
-CC NAME       :  chkbrdc
+CC NAME       :  chkbrdc_gps
 CC
-CC PURPOSE    :  CHECK TWO BROADCAST MESSAGES FOR CONSISTENCY
+CC PURPOSE    :  CHECK ONE EPHEMERIS MESSAGE FOR GARBAGE. SET STATUS
+CC               ACCORDING TO THE RESULT OF THE CHECK
 CC
 CC PARAMETERS :
-CC         IN :  EPH1   : EPHEMERIDES INFORMATION (FIRST SET)  R*8(20)
-CC                        EPH1(I):
+CC         IN :  EPH    : EPHEMERIDES INFORMATION              R*8(20)
+CC                        EPH(I):
 CC                          I: EPHEMERIDE ELEMENT
-CC                          EPH1(1) : GPS-WEEK
-CC                          EPH1(2) : T0E
-CC                          EPH1(3) : A
-CC                          EPH1(4) : E
-CC                          EPH1(5) : I
-CC                          EPH1(6) : R.A. OF ASCENDING NODE
-CC                          EPH1(7) : PERIGEE
-CC                          EPH1(8) : MEAN ANOMALY (T0E)
-CC                          EPH1(9) : DN (CORRECTION TO MEAN MOTION)
-CC                          EPH1(10): RATE OF NODE
-CC                          EPH1(11): CUS
-CC                          EPH1(12): CUC
-CC                          EPH1(13): CRS
-CC                          EPH1(14): CRC
-CC                          EPH1(15): CIS
-CC                          EPH1(16): CIC
-CC                          EPH1(17): AODE
-CC                          EPH1(18): IDOT
-CC                          EPH1(19): NOT USED
-CC                          EPH1(20): NOT USED
-CC                          EPH1(21): GPS WEEK OF THE NEXT EPHEMERIDE
+CC                          EPH(1) : GPS-WEEK
+CC                          EPH(2) : T0E
+CC                          EPH(3) : A
+CC                          EPH(4) : E
+CC                          EPH(5) : I
+CC                          EPH(6) : R.A. OF ASCENDING NODE
+CC                          EPH(7) : PERIGEE
+CC                          EPH(8) : MEAN ANOMALY (T0E)
+CC                          EPH(9) : DN (CORRECTION TO MEAN MOTION)
+CC                          EPH(10): RATE OF NODE
+CC                          EPH(11): CUS
+CC                          EPH(12): CUC
+CC                          EPH(13): CRS
+CC                          EPH(14): CRC
+CC                          EPH(15): CIS
+CC                          EPH(16): CIC
+CC                          EPH(17): AODE
+CC                          EPH(18): IDOT
+CC                          EPH(19): NOT USED
+CC                          EPH(20): NOT USED
+CC                          EPH(21): GPS WEEK OF THE NEXT EPHEMERIDE
 CC                              :        :
-CC               EPH2   : EPHEMERIDES INFORMATION (SECOND SET) R*8(20)
-CC                          (SEE ARRAY EPH1)
+CC        OUT :  STATUS : STATUS OF THE MESSAGE (RESULT OF    CH*8
+CC                          THE CHECKS PERFORMED)
 CC
-CC
-C*
       IMPLICIT NONE
 C
 C DECLARATIONS INSTEAD OF IMPLICIT
 C --------------------------------
-      INTEGER*4 IWEEK1, IWEEK2
+      INTEGER*4 IWEEK, N
 C
-      REAL*8    A1    , A2    , DA    , DE    , DI    , DM0   , DN1   ,
-     1          DNODE , DPER  , DT    , E1    , E2    , HOURS , PER1  ,
-     2          PER2  , PER21 , T0E1  , T0E2  , XI1   , XI2   , XIDOT1,
-     3          XM01  , XM02  , XM021 , XN    , XNDOT1, XNOD21, XNODE1,
-     4          XNODE2, XNODEW
+      REAL*8    A    , DN   , E    , ODOT , PER  , T0E  , XI   , XM0  ,
+     1          XNODE, PI
 C
-CCC       IMPLICIT REAL*8(A-H,O-Z)
-CCC       IMPLICIT INTEGER*4 (I-N)
-      REAL*8      EPH1(20),EPH2(20)
+      REAL*8      EPH(20),AVE(8),STD(8)
       CHARACTER*8 STATUS
-      REAL*8 GM, OMEGA, PI
 C
+C
+C INITIALIZE STATUS
+C -----------------
+      STATUS=' '
       PI = 4*ATAN(1.D0)
-      OMEGA  = 7292115.1467D-11 ! RAD/SEC
-      GM     = 398.6004415D12   !  M**3/SEC**2
-
-C
-C
-C FIRST SET OF ELEMENTS
-C ---------------------
-      IWEEK1=IDNINT(EPH1(1))
-      T0E1  =EPH1(2)
-      A1    =EPH1(3)
-      E1    =EPH1(4)
-      XI1   =EPH1(5)*180.D0/PI
-      XNODE1=EPH1(6)*180.D0/PI
-      PER1  =EPH1(7)*180.D0/PI
-      XM01  =EPH1(8)*180.D0/PI
-      DN1   =EPH1(9)
-      XNDOT1=EPH1(10)
-      XIDOT1=EPH1(18)
-C
-C SECOND SET OF ELEMENTS
+      N = 4
+C      
+C SET EPHEMERIS ELEMENTS
 C ----------------------
-      IWEEK2=IDNINT(EPH2(1))
-      T0E2  =EPH2(2)
-      A2    =EPH2(3)
-      E2    =EPH2(4)
-      XI2   =EPH2(5)*180.D0/PI
-      XNODE2=EPH2(6)*180.D0/PI
+      IWEEK=IDNINT(EPH(1))
+      T0E  =EPH(2)
+      A    =EPH(3)
+      E    =EPH(4)
+      XI   =EPH(5)*180/PI
+      XNODE=EPH(6)*180/PI
+      PER  =EPH(7)*180/PI
+      XM0  =EPH(8)*180/PI
+      DN   =EPH(9)
+      ODOT =EPH(10)*180/PI
 C
-C CORRECT NODE FOR SYSTEM CHANGE AT END OF GPS WEEK
-      IF(IWEEK1.NE.IWEEK2) THEN
-        XNODEW=DMOD((IWEEK2-IWEEK1)*86400.D0*7.D0*OMEGA*180.D0
-     1              /PI,360.D0)
-        XNODE2=XNODE2+XNODEW
-      ENDIF
-      PER2  =EPH2(7)*180.D0/PI
-      XM02  =EPH2(8)*180.D0/PI
-      IF(XM02.LE.-180.D0) XM02=XM02+360.D0
-      IF(XM02.GT.+180.D0) XM02=XM02-360.D0
-C
-C COMPUTE CHANGE IN FIRST ELEMENTS IN THE TIME INTERVAL (T0E1,T0E2)
-C -----------------------------------------------------------------
-      DT=(IWEEK2-IWEEK1)*86400.D0*7+T0E2-T0E1
-      HOURS=DT/3600.D0
-      XI1=XI1+XIDOT1*DT*180.D0/PI
-      XNODE1=XNODE1+XNDOT1*DT*180.D0/PI
-      XN=DSQRT(GM/A1**3)+DN1
-      XM01=XM01+XN*DT*180.D0/PI
-      IF(XM01.LE.-180.D0) XM01=XM01+360.D0
-      IF(XM01.GT.+180.D0) XM01=XM01-360.D0
-C
-C CHECK DIFFERENCE IN A
-C ---------------------
-      DA=100.D0+10.D0*HOURS
-      IF(DABS(A2-A1).GT.DA) THEN
-        PRINT*,'BAD DA'
+C CHECK GPS WEEK
+C --------------
+      IF (IWEEK == 0) RETURN
+      IF(IWEEK.LE.0.OR.IWEEK.GT.2000) THEN
+        STATUS='BAD WEEK'
+        PRINT*,'STATUS =', STATUS, 'GPSWEEK =', IWEEK
         GOTO 100
       ENDIF
 C
-C CHECK DIFFERENCE IN E
-C ---------------------
-      DE=5.D-6+0.5D-6*HOURS
-      IF(DABS(E2-E1).GT.DE) THEN
-        PRINT*,'BAD DE'
+C CHECK T0E
+C ---------
+!      IF(T0E.LT.0.D0 .OR. T0E.GT.7*86400.D0 .OR.
+!     1   (DABS(DMOD(T0E,100.D0)-3.D0).GT.1.D-5 .AND.
+!     2   (DMOD(T0E+1.D-5,100.D0)).GT.2.D-5)) THEN
+!        STATUS='BAD T0E'
+!        PRINT*,'STATUS =', STATUS
+!        GOTO 100
+!      ENDIF
+C
+C CHECK A
+C -------
+      IF (A == 0.d0) RETURN
+!      IF(A.LT.26.0D6.OR.A.GT.27.0D6) THEN
+      IF(ABS(A-AVE(1))>N*STD(1))THEN
+        STATUS='BAD A'
+        PRINT*,'STATUS =',STATUS
+        PRINT*,'ABS(OBSERVED-MEAN)',ABS(A-AVE(1))
         GOTO 100
       ENDIF
 C
-C CHECK DIFFERENCE IN I
-C ---------------------
-      DI=0.0005D0+0.0003D0*HOURS
-      IF(DABS(XI2-XI1).GT.DI) THEN
-        PRINT*,'BAD DI'
+C CHECK E
+C -------
+!      IF(E.LT.0.D0.OR.E.GT.0.1D0) THEN
+      IF(ABS(E-AVE(2))>N*STD(2))THEN
+        STATUS='BAD E'
+        PRINT*,'STATUS =',STATUS
+        PRINT*,'ABS(OBSERVED-MEAN)',ABS(E-AVE(2))
         GOTO 100
       ENDIF
 C
-C CHECK DIFFERENCE IN NODE
-C ------------------------
-      XNOD21=DABS(XNODE2-XNODE1)
-      IF (XNOD21.GT.100.D0) XNOD21=DABS(XNOD21-360.D0)
-      DNODE=0.0002D0+0.0005D0*HOURS
-      IF(XNOD21.GT.DNODE) THEN
-        PRINT*,'BAD DNOD'
+C CHECK I
+C -------
+      !IF((XI.GT.65.D0.OR.XI.LT.60.D0).AND.(XI.GT.58.OR.XI.LT.50)) THEN
+      IF(ABS(XI-AVE(3)*180/PI)>N*STD(3)*180/PI)THEN
+        STATUS='BAD I'
+        PRINT*,'STATUS =',STATUS
+        PRINT*,'ABS(OBSERVED-MEAN)',ABS(XI-AVE(3)*180/PI)
         GOTO 100
       ENDIF
 C
-C CHECK DIFFERENCE IN PERIGEE
-C ---------------------------
-      PER21=DABS(PER2-PER1)
-      IF (PER21.GT.100.D0) PER21=DABS(PER21-360.D0)
-      DPER=2.0D0+0.01D0*HOURS
-      IF (EPH1(4).LT.0.001) DPER=10.0D0+0.01D0*HOURS
-      IF(PER21.GT.DPER) THEN
-        PRINT*,'BAD DPER'
+C CHECK NODE
+C ----------
+      !IF(DABS(XNODE).LT.1.D-6) THEN
+      IF(ABS(XNODE-AVE(4)*180/PI)>N*STD(4)*180/PI)THEN
+        STATUS='BAD NODE'
+        PRINT*,'STATUS =',STATUS
+        PRINT*,'ABS(OBSERVED-MEAN)',ABS(XNODE-AVE(4)*180/PI)
         GOTO 100
       ENDIF
 C
-C CHECK DIFFERENCE IN MEAN ANOMALY
-C --------------------------------
-      XM021=DABS(XM02-XM01)
-      IF(XM021.GT.100.D0) XM021=DABS(XM021-360.D0)
-      DM0=2.00D0+0.08D0*HOURS
-      IF (EPH1(4).LT.0.001) DM0=10.0D0+0.01D0*HOURS
-      IF(XM021.GT.DM0) THEN
-        PRINT*,'BAD DM0'
+C CHECK PERIGEE
+C -------------
+      !IF(DABS(PER).LT.1.D-6) THEN
+      IF(ABS(PER-AVE(5)*180/PI)>N*STD(5)*180/PI)THEN
+        STATUS='BAD PERI'
+        PRINT*,'STATUS=',STATUS
+        PRINT*,'ABS(OBSERVED-MEAN)',ABS(PER-AVE(5)*180/PI)
+        GOTO 100
+      ENDIF
+C
+C CHECK MEAN ANOMALY
+C ------------------
+      !IF(DABS(XM0).LT.1.D-6) THEN
+      IF(ABS(XM0-AVE(6)*180/PI)>N*STD(6)*180/PI)THEN
+        STATUS='BAD M0'
+        PRINT*,'STATUS=',STATUS
+        PRINT*,'ABS(OBSERVED-MEAN)',ABS(XM0-AVE(6)*180/PI)
+        GOTO 100
+      ENDIF
+C
+C CHECK CORRECTION TO MEAN MOTION
+C -------------------------------
+      !IF(DN.LT.0.01D-8.OR.DN.GT.0.70D-8) THEN
+      IF(ABS(DN-AVE(7))>N*STD(7))THEN
+        STATUS='BAD DN'
+        PRINT*,'STATUS =',STATUS
+        PRINT*,'ABS(OBSERVED-MEAN)', ABS(DN-AVE(7))
+        GOTO 100
+      ENDIF
+C
+C CHECK RATE OF NODE
+C ------------------
+      !IF(ODOT.LT.-0.60D-6.OR.ODOT.GT.-0.25D-6) THEN
+      IF(ABS(ODOT-AVE(8)*180/PI)>N*STD(8)*180/PI)THEN
+        STATUS='BAD ODOT'
+        PRINT*,'STATUS=',STATUS
+        PRINT*,'ABS(OBSERVED-MEAN)',ABS(ODOT-AVE(8)*180/PI)
         GOTO 100
       ENDIF
 C
