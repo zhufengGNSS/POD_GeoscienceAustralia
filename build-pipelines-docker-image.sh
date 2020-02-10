1b6c7367dbcdc95ffb90f4e0c7988e28f07f1896
Michael Moore <michael.moore@ga.gov.au>
2020-02-10 22:25:21 +1100

    update to build-pipeline

--- build-pipelines-docker-image.sh
+++ build-pipelines-docker-image.sh
@@ -9,5 +9,6 @@ organization=gamichaelmoore
 repository=pod-pipelines
 
 docker login -u "$DOCKER_HUB_USERNAME" -p "$DOCKER_HUB_PASSWORD"
-docker build -t "$organization/$repository" --build-arg AWS_SECRET_KEY="$AWS_SECRET_KEY_ID" --build-arg AWS_ACCESS_KEY="$AWS_ACCESS_KEY_ID" -f bitbucket-pipelines.dockerfile .
+#docker build -t "$organization/$repository" --build-arg AWS_SECRET_KEY="$AWS_SECRET_KEY_ID" --build-arg AWS_ACCESS_KEY="$AWS_ACCESS_KEY_ID" -f bitbucket-pipelines.dockerfile .
+docker build -t "$organization/$repository" -f bitbucket-pipelines.dockerfile .
 docker push "$organization/$repository"
